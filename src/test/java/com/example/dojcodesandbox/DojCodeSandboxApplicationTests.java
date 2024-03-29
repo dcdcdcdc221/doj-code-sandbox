5d0336bbde3b88f2f59d5cad840488f03447df2e
dcdcdcdc221 <email>
2024-03-29 16:50:33 +0800

    '20240329'

--- src/test/java/com/example/dojcodesandbox/DojCodeSandboxApplicationTests.java
+++ src/test/java/com/example/dojcodesandbox/DojCodeSandboxApplicationTests.java
@@ -4,10 +4,27 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DojCodeSandboxApplicationTests {
+class TimeOutThread  extends Thread{
 
-    @Test
-    void contextLoads() {
+
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(3000);
+            System.out.println("超时结束了");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        DeamonThread t1 = new DeamonThread();
+        TimeOutThread t2 = new TimeOutThread();
+        t1.setDaemon(true);
+        t1.start();
+        t2.start();
+
     }
 
 }
