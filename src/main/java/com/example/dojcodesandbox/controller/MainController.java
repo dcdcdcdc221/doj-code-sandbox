4c362ac5821d87654bc048cd5c33f106b47692ae
dcdcdcdc221 <email>
2024-03-29 15:29:42 +0800

    ''

--- src/main/java/com/example/dojcodesandbox/controller/MainController.java
+++ src/main/java/com/example/dojcodesandbox/controller/MainController.java
@@ -6,20 +6,9 @@ import javax.annotation.Resource;
 
 @RestController("/")
 public class MainController {
-<<<<<<< HEAD
     //定义鉴权请求头和密钥
     private static final String AUTH_REQUEST_HEADER = "auth";
 
     private static final String AUTH_REQUEST_SECRET = "secretKey";
 
-    @Resource
-=======
-    //定义健全请求头和密钥
-    private static final String AUTH_REQUEST_HEADER = "auth";
-
-    private static final String AUTH_REQUEST_KEY = "secretKey";
-
-    @Resource
-    private JavaNative
->>>>>>> 3cf484a0f6c3261106140b616b1e5b81e3d8f837
 }
